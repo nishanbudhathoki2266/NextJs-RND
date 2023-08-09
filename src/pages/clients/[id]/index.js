4093169361a1d42dc86351fdcc1f569ff7caaebb
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-09 21:34:09 +0545

    Learned about navigating programatically

--- src/pages/clients/[id]/index.js
+++ src/pages/clients/[id]/index.js
@@ -4,9 +4,18 @@ const ClientProjectsPage = () => {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "max", clientProjectId: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
