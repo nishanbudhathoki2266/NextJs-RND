c64860b6ac614bf9265b5fa2602f4421da941eee
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-09 21:27:46 +0545

    Navigated to dynamic routes

--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "nishan", name: "Nishan Budhathoki" },
+    { id: "max", name: "Maximillian Thapa" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
