2f6e26c351009f229424ec4d5ab613d90bc3953d
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-10 11:07:48 +0545

    Designed the event detail page

--- event-manager/pages/events/[eventId].js
+++ event-manager/pages/events/[eventId].js
@@ -1,12 +1,26 @@
+import EventContent from "@/components/eventDetail/EventContent";
+import EventLogistics from "@/components/eventDetail/EventLogistics";
+import EventSummary from "@/components/eventDetail/EventSummary";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { Fragment } from "react";
 
 const EventDetailPage = () => {
+  const router = useRouter();
+  const event = getEventById(router.query.eventId);
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
