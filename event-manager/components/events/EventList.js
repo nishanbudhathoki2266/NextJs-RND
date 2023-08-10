c8848a97b2b4ba21751f6f47896980bc0860c275
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-10 10:25:29 +0545

    Added basic stylings and components

---
+++ event-manager/components/events/EventList.js
@@ -0,0 +1,15 @@
+import EventItem from "./EventItem";
+import classes from "./EventList.module.css";
+
+function EventList(props) {
+  const { items } = props;
+  return (
+    <ul className={classes.list}>
+      {items.map((event) => (
+        <EventItem id={event.id} event={event} />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
