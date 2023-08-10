28c44a786129065b3517e18a0f41fa00f003551d
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-10 13:17:34 +0545

    Extracted data on the catch all page

--- event-manager/pages/events/[...slug].js
+++ event-manager/pages/events/[...slug].js
@@ -1,6 +1,40 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import React from "react";
 
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) return <p className="center">Loading...</p>;
+
+  const filteredYear = filterData[0];
+  const filterMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filterMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please provide valid values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
