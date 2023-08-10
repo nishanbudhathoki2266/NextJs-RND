7f4b394b6377858f34727b1d69a3751f5848dce6
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-10 13:05:27 +0545

    Added programmatic navigation to the filtered event page

--- event-manager/components/events/EventsSearch.js
+++ event-manager/components/events/EventsSearch.js
@@ -1,13 +1,25 @@
+import { useRef } from "react";
 import Button from "../ui/Button";
 import classes from "./EventsSearch.module.css";
 
 function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select ref={yearInputRef} id="year">
             <option value="2022">2022</option>
             <option value="2023">2023</option>
           </select>
@@ -15,7 +27,7 @@ function EventsSearch(props) {
 
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select ref={monthInputRef} id="month">
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- event-manager/pages/events/index.js
+++ event-manager/pages/events/index.js
@@ -1,13 +1,22 @@
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+
 import EventList from "@/components/events/EventList";
 import EventsSearch from "@/components/events/EventsSearch";
 import { getAllEvents } from "@/dummy-data";
-import React, { Fragment } from "react";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
