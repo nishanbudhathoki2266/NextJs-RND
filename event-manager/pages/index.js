cf910c2bea09a8f15677b6e2b207e4ea9f748985
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-11 11:14:29 +0545

    Played around with Head tag given by Next js

--- event-manager/pages/index.js
+++ event-manager/pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import React from "react";
 import { getFeaturedEvents } from "./../helpers/api-util";
 import EventList from "@/components/events/EventList";
@@ -5,6 +7,13 @@ import EventList from "@/components/events/EventList";
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of greate events that allow you to evolve."
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
