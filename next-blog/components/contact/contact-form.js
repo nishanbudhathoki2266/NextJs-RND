84d664359cda9386cc9b33b495e0c4da67b1a9d8
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-12 14:27:24 +0545

    Fixed a little typo in the class name of form

--- next-blog/components/contact/contact-form.js
+++ next-blog/components/contact/contact-form.js
@@ -4,7 +4,7 @@ function ContactForm() {
   return (
     <section className={classes.contact}>
       <h1>How can I help you ?</h1>
-      <form className={classes.fomr}>
+      <form className={classes.form}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
@@ -20,6 +20,7 @@ function ContactForm() {
           <label htmlFor="message">Your Message</label>
           <textarea id="message" rows="5"></textarea>
         </div>
+
         <div className={classes.actions}>
           <button>Send Message</button>
         </div>
