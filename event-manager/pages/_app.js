2923bcdb1c81275a1731a1edf8c59d8ba61083ac
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-12 10:14:16 +0545

    Added a notification context

--- event-manager/pages/_app.js
+++ event-manager/pages/_app.js
@@ -1,16 +1,28 @@
 import Layout from "@/components/layout/Layout";
+import Notification from "@/components/ui/Notification";
+import { NotificationContextProvider } from "@/store/notification-context";
 import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="Next Js event manager" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />;
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="Next Js event manager" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />;
+        <Notification
+          title="test"
+          message="This is a test message"
+          status="pending"
+        />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
