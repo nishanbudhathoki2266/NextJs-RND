04effdd2ac7ee5260e2a4ae7fb8b92d2b18afb6e
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-12 17:41:06 +0545

    Implemented registering a user

--- next-authentication/pages/api/auth/signup.js
+++ next-authentication/pages/api/auth/signup.js
@@ -2,33 +2,39 @@ import { hashPassword } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
 
 async function handler(req, res) {
-  const { email, password } = req.body.data;
+  if (req.method === "POST") {
+    const { email, password } = req.body;
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    return res.status(422).json({
-      message: "Invalid input!",
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      return res.status(422).json({
+        message: "Invalid input!",
+      });
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db();
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "Created user!",
     });
   }
 
-  const client = await connectToDatabase();
-
-  const db = client.db();
-
-  const hashedPassword = hashPassword(password);
-
-  const result = await db.collection("users").insertOne({
-    email: email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({
-    message: "Created user!",
-  });
+  if (req.method === "GET") {
+    console.log("I am coming from get request");
+  }
 }
 
 export default handler;
