de105d749fa327cc951ce07550865b11a40610f1
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-12 11:22:12 +0545

    Added logo and stylings

--- next-blog/components/layout/main-navigation.js
+++ next-blog/components/layout/main-navigation.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
 import Logo from "./logo";
+import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
   return (
-    <header>
+    <header className={classes.header}>
       <Link href="/">
         {/* Remember that you must pass text inside Link, so we are wrapping <Logo /> inside an anchor tag */}
         <a>
