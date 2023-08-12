c0100515565929642c273a985e75043eabb2b9d6
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-12 12:06:02 +0545

    Rendered dummy post data

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,10 +1,47 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack applications too easy and less cumbersome.",
+    date: "2023-05-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with ReactJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack applications too easy and less cumbersome.",
+    date: "2023-05-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack applications too easy and less cumbersome.",
+    date: "2023-05-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack applications too easy and less cumbersome.",
+    date: "2023-05-10",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
